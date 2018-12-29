b1828af5563d8ec3be892e3569750c060fb57a81
Pedro Correa <pedro8correa@gmail.com>
2018-12-29 15:54:29 -0200

    Added mock heroes list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Mr. Nice' },
+  { id: 2, name: 'Narco' },
+  { id: 3, name: 'Bombasto' },
+  { id: 4, name: 'Celeritas' },
+  { id: 5, name: 'Magneta' },
+  { id: 6, name: 'RubberMan' },
+];
