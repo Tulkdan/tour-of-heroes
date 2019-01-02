0afbff59f4f753796487892c3b4b11ad04cff870
Pedro Correa <pedro8correa@gmail.com>
2019-01-01 23:23:39 -0200

    Added in memory data service and created its service

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService {
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Mr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Magneta' },
+      { id: 15, name: 'Celeritas' },
+      { id: 16, name: 'RubberMan' },
+    ];
+    return {heroes};
+  }
+
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+
+  constructor() { }
+}
